de61b8b279d8bd6c3d69fd3c87f1be6b1343de3a
robert-waelchli <rwaelchli@student.cccs.edu>
2024-11-28 14:56:21 -0700

    Initial Commit

---
+++ cspb_2270_final_project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cspb_2270_final_project
+//
+//  Created by Robert Waelchli on 11/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
